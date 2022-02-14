bacc5617b35208feef8087538edc0f3ee11e5dfb
rar <davidtri2013@yandex.ru>
2022-02-14 23:17:55 +0300

    Refactoring

--- app/src/main/java/com/example/newsfeedapp/ApiService.kt
+++ app/src/main/java/com/example/newsfeedapp/ApiService.kt
@@ -1,9 +1,23 @@
-
 import com.example.testapp.model.QueryResult
-import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+
+const val BASE_URL = "https://ws-tszh-1c-test.vdgb-soft.ru/api/mobile/news/list/"
 
 interface ApiService {
     @GET(BASE_URL)
     suspend fun getInfo(): QueryResult
+}
+
+fun getQueryResult() : ApiService{
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    // Передача имени класса и получение сервиса
+    val service: ApiService = retrofit.create(ApiService::class.java)
+
+    return service
 }
